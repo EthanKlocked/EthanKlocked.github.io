2e671789e17ae79ff747d9b62e9dba2f831334f3
ethank2020 <wnlwnl2003@gmail.com>
2021-07-24 16:40:44 +0900

    update tag

--- js/tags.js
+++ js/tags.js
@@ -1,15 +1,40 @@
-$("[data-tag]").click((e) => {
-    currentTag = e.target.dataset.tag;
-    filterByTagName(currentTag);
-  })
-  
-  function filterByTagName(tagName) {
-    $('.hidden').removeClass('hidden');
-    $('.post-wrapper').each((index, elem) => {
-      if (!elem.hasAttribute(`data-${tagName}`)) {
-        $(elem).addClass('hidden');
-      }
-    });
-    $(`.tag`).removeClass('selected');
-    $(`.tag[data-tag=${tagName}]`).addClass('selected');
+$(document).ready(function() {
+  let currentTag = "";  
+  var url_string = window.location.href;
+  var url = new URL(url_string);
+  var tag_nm = url.searchParams.get("tag");
+  if (tag_nm) {
+    currentTag = tag_nm;
+    filterByTagName(currentTag)
   }
+});
+
+$("[data-tag]").click((e) => {
+  currentTag = e.target.dataset.tag;
+  filterByTagName(currentTag);
+})
+
+function tag_post(data){
+  location.href = '/posts?tag='+data+'';
+}
+
+function filterByTagName(tagName) {
+  $('.hidden').removeClass('hidden');
+  $('.post-wrapper').each((index, elem) => {
+    if (!elem.hasAttribute(`data-${tagName}`)){
+      $(elem).addClass('hidden');
+    }
+  });
+  $(`.tag`).removeClass('selected');
+  $(`.tag[data-tag=${tagName}]`).addClass('selected');
+}
+
+function getUrlParams() {     
+  var params = {};  
+  window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, 
+    function(str, key, value) { 
+        params[key] = value; 
+      }
+  );      
+  return params; 
+}
